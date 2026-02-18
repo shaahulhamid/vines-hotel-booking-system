3db6f212fc2cf2d6a7d54232f8ee762d72db0470
Shaahul hamid kassim <shaahulhamid.kassim@gmail.com>
2026-02-18 17:41:03 +0530

    Modified securityconfig removed inMemoryDetailsManager and added CustomerUserDetailsSevice

--- vines-hotel-booking/src/main/java/com/vineshotel/config/SecurityConfig.java
+++ vines-hotel-booking/src/main/java/com/vineshotel/config/SecurityConfig.java
@@ -2,14 +2,11 @@ package com.vineshotel.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,14 +15,6 @@ public class SecurityConfig {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
-	public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
-		UserDetails admin = User.builder().username("admin").password(passwordEncoder.encode("admin123")).roles("ADMIN")
-				.build();
-
-		return new InMemoryUserDetailsManager(admin);
 	}
 
 	@Bean
@@ -46,7 +35,7 @@ public class SecurityConfig {
 		
 		.formLogin( form -> form
 				.loginPage("/login")
-				.defaultSuccessUrl("/home",true)
+				.defaultSuccessUrl("/",true)
 				.permitAll()
 				)
 		.logout( logout -> logout
